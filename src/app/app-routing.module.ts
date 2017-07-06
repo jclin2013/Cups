a0b4c713bafb28f7a5c601c153b197e3a684d19e
Jonathan Lin <jclin2013@gmail.com>
2017-07-06 16:42:40 -0400

    Begin layout work, get cup image on form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     path: '',
     redirectTo: '/cupsform',
     pathMatch: 'full'
-  },
+  }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>CUPS</h1>
-    <nav>
-      <a routerLink="/cupsform">Add a New Cup</a>
-      <a routerLink="/cups-table">Cups</a>
-      <a routerLink="/dashboard">Dashboard</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+    <div id='consoleContainer'>
+      <h1>CUPS</h1>
+      <nav>
+        <a routerLink="/cupsform" routerLinkActive="active">Add a New Cup</a>
+        <a routerLink="/cups-table" routerLinkActive="active">Cups</a>
+        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      </nav>
+      <router-outlet></router-outlet>
+    </div>
+  `,
+
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
 }
+
+//<div ng-init="myImage = 'cup.png'">
+//</div>
