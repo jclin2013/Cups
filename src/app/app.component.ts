f27d2f9b430c969634fddfe9a5e7a02b006245a5
Jonathan Lin <jclin2013@gmail.com>
2017-07-06 02:18:05 -0400

    Make initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+export class Cup {
+  id: number;
+  material: string;
+  name: string;
+  display: string;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent  {
+  title = 'Cups';
+  cup: Cup = {
+    id: 1,
+    name: 'Starbucks'
+  }
+}
